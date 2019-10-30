6e609004832806dee77811980256dc24d941a84b
heleneduputie <heleneduputie1993@gmail.com>
2019-10-30 16:42:28 +0000

    add album database and transaction

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,12 +1,31 @@
 package org.superbiz.moviefun;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@SpringBootApplication
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -15,5 +34,68 @@ public class Application {
     @Bean
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+
+    @Bean
+    public DatabaseServiceCredentials databaseServiceCredentials(@Value("${VCAP_SERVICES}") String vcap){
+        return new DatabaseServiceCredentials(vcap);
+    }
+
+    @Bean
+    public DataSource albumsDataSource(DatabaseServiceCredentials serviceCredentials) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(serviceCredentials.jdbcUrl("albums-mysql"));
+        return dataSource;
+    }
+
+    @Bean
+    public DataSource moviesDataSource(DatabaseServiceCredentials serviceCredentials) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(serviceCredentials.jdbcUrl("movies-mysql"));
+        return dataSource;
+    }
+
+    @Bean
+    public HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
+        HibernateJpaVendorAdapter mySqlHibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        mySqlHibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        mySqlHibernateJpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+        mySqlHibernateJpaVendorAdapter.setGenerateDdl(true);
+        return mySqlHibernateJpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean movieLocalContainerEntityManagerFactoryBean(
+            @Qualifier("moviesDataSource") DataSource dataSource,
+            HibernateJpaVendorAdapter hibernateJpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("org.superbiz.moviefun");
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("movieFactory");
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean albumLocalContainerEntityManagerFactoryBean(
+            @Qualifier("albumsDataSource") DataSource dataSource,
+            HibernateJpaVendorAdapter hibernateJpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("org.superbiz.moviefun");
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("albumFactory");
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager moviePlatformTransactionManager(EntityManagerFactory movieLocalContainerEntityManagerFactoryBean){
+
+        return new JpaTransactionManager(movieLocalContainerEntityManagerFactoryBean);
+    }
+
+    @Bean
+    public PlatformTransactionManager albumPlatformTransactionManager(EntityManagerFactory albumLocalContainerEntityManagerFactoryBean){
+        return new JpaTransactionManager(albumLocalContainerEntityManagerFactoryBean);
     }
 }
--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,6 +1,11 @@
 package org.superbiz.moviefun;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.superbiz.moviefun.albums.Album;
 import org.superbiz.moviefun.albums.AlbumFixtures;
@@ -18,12 +23,26 @@ public class HomeController {
     private final AlbumsBean albumsBean;
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
+    private PlatformTransactionManager moviePlatformTransactionManager;
+    private PlatformTransactionManager albumPlatformTransactionManager;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    private final TransactionTemplate movieTransactionTemplate;
+    private final TransactionTemplate albumTransactionTemplate;
+
+    public HomeController(MoviesBean moviesBean,
+                          AlbumsBean albumsBean,
+                          MovieFixtures movieFixtures,
+                          AlbumFixtures albumFixtures,
+                          @Qualifier("moviePlatformTransactionManager") PlatformTransactionManager moviePlatformTransactionManager,
+                          @Qualifier("albumPlatformTransactionManager") PlatformTransactionManager albumPlatformTransactionManager) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
         this.albumFixtures = albumFixtures;
+        this.moviePlatformTransactionManager = moviePlatformTransactionManager;
+        this.albumPlatformTransactionManager = albumPlatformTransactionManager;
+        this.movieTransactionTemplate = new TransactionTemplate(moviePlatformTransactionManager);
+        this.albumTransactionTemplate = new TransactionTemplate(albumPlatformTransactionManager);
     }
 
     @GetMapping("/")
@@ -33,12 +52,26 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
+
         for (Movie movie : movieFixtures.load()) {
-            moviesBean.addMovie(movie);
+            movieTransactionTemplate.execute(new TransactionCallback() {
+                // the code in this method executes in a transactional context
+                public Object doInTransaction(TransactionStatus status) {
+                    moviesBean.addMovie(movie);
+                    return null;
+                }
+            });
+
         }
 
         for (Album album : albumFixtures.load()) {
-            albumsBean.addAlbum(album);
+            albumTransactionTemplate.execute(new TransactionCallback() {
+                // the code in this method executes in a transactional context
+                public Object doInTransaction(TransactionStatus status) {
+                    albumsBean.addAlbum(album);
+                    return null;
+                }
+            });
         }
 
         model.put("movies", moviesBean.getMovies());
