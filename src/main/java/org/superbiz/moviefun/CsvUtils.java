45bef880daaafef699a2a76ce4f29581d3263a32
heleneduputie <heleneduputie1993@gmail.com>
2019-10-31 09:58:01 +0000

    Classreader added

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -3,9 +3,8 @@ package org.superbiz.moviefun;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,18 +12,17 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
+        InputStream inputStream = CsvUtils.class.getClassLoader().getResourceAsStream(path);
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
